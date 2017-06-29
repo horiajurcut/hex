db70bbc136caf05dfb79a1265d53d3091d06ff71
Horia Jurcut <horia.dragos@gmail.com>
2017-06-29 21:09:07 +0200

    Implement Clock Animator scene

--- Hex/Assets/Scripts/Tutorials/ClockAnimator.cs
+++ Hex/Assets/Scripts/Tutorials/ClockAnimator.cs
@@ -1,16 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
-public class ClockAnimator : MonoBehaviour {
+namespace Tutorials
+{
+	public class ClockAnimator : MonoBehaviour
+	{
+		public Transform Hours, Minutes, Seconds;
+		public bool Analog;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		private const float HoursToDegrees = 360f / 12f;
+		private const float MinutesToDegrees = 360f / 60f;
+		private const float SecondsToDegrees = 360f / 60f;
+
+		private void Update()
+		{
+			if (Analog)
+			{
+				var timeSpan = DateTime.Now.TimeOfDay;
+				
+				Hours.localRotation = Quaternion.Euler(0f, 0f, (float)timeSpan.TotalHours * -HoursToDegrees);
+				Minutes.localRotation = Quaternion.Euler(0f, 0f, (float)timeSpan.TotalMinutes * -MinutesToDegrees);
+				Seconds.localRotation = Quaternion.Euler(0f, 0f, (float)timeSpan.TotalSeconds * -SecondsToDegrees);
+			}
+			else
+			{
+				var time = DateTime.Now;
+			
+				Hours.localRotation = Quaternion.Euler(0f, 0f, time.Hour * -HoursToDegrees);
+				Minutes.localRotation = Quaternion.Euler(0f, 0f, time.Minute * -MinutesToDegrees);
+				Seconds.localRotation = Quaternion.Euler(0f, 0f, time.Second * -SecondsToDegrees);
+			}
+		}
 	}
 }
